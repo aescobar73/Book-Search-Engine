926d9faa041421dda4698e88b7ba489887ad40be
Angel Escobar <escobar@Angels-MacBook-Pro-162.local>
2022-11-15 01:17:33 -0600

    client compiled without errors. Work on server errors

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -22,8 +22,63 @@ export const LOGIN_USER = gql`
     }
 `
 
-export const ADD_USER = gql``
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookID
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
+    }
+`
 
-export const SAVE_BOOK = gql``
+export const SAVE_BOOK = gql`
+    mutation saveBook ($input: bookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`
 
-export const REMOVE_BOOK = gql``
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookID: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+
+        }
+    }
+`
