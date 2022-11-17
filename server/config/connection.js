3e327bf68a205823e853155503224641c0d424c9
Angel Escobar <escobar@Angels-MacBook-Pro-162.local>
2022-11-16 22:26:39 -0600

    error fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nuBoot45', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nuBoot45', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
